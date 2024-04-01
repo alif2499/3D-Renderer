46b57e947903a923c8c0e18e70abbd1998400005
alif2499 <alifjcc99@gmail.com>
2024-04-01 17:20:55 -0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+// Default app creation code
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// Merged code from online to render 3D data 
+
+import Box from "@mui/material/Box";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import AppBarTop from "./Components/AppBarTop";
+import Renderer from "./Components/RenderPaper";
+import AppBarBottom from "./Components/AppBarBottom";
+import DataRender from "./Components/DataRender";
+import "./App.css";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#5DADE2",
+    },
+  },
+  typography: {
+    fontFamily: ["Gill Sans", "Nunito", "sans-serif"].join(","),
+    fontWeightLight: 300,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          minHeight: "100vh",
+          minWidth: "100vw",
+          display: "flex",
+          justifyContent: "flex-start",
+          flexDirection: "column",
+          backgroundColor: "#111418",
+        }}
+      >
+        <AppBarTop />
+        {/* <Renderer /> */}
+        <DataRender />
+        <AppBarBottom />
+      </Box>
+    </ThemeProvider>
   );
 }
-
 export default App;
+
+
+
+
+
+// For practicing to render 3D data manually
+
+// import React from 'react';
+// // import MedicalImageViewer from './MedicalImageViwer';
+// import VtkViewerr from './VtkViewerr';
+// // import DicomViewer from './DicomViewer';
+
+// function App() {
+//   // Define the DICOM dataset URL or binary data
+//   const dataPath = 'src\\teapot.stl';
+
+//   return (
+//     <div>
+//       <h1>VTK.js 3D Viewer</h1>
+//       <VtkViewerr dataUrl={dataPath} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
